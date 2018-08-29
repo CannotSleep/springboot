3a9781b3e0b1ad698bb1f0c9e816f5311422cf4f
Zyt <zyt_dsg@163.com>
2018-08-29 17:00:11 +0800

    1

---
+++ src/main/java/com/zzh/demo1/HelloController.java
@@ -0,0 +1,22 @@
+package com.zzh.demo1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${name}")
+    private String name;
+
+    @Value("${age}")
+    private Integer age;
+
+    @RequestMapping("/hello")
+    public String hello(){
+
+        return name + age;
+    }
+
+}
